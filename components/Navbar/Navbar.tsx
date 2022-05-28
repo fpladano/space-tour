08a0d1367c38f30fb869e056012efa2ecc1329e7
Francisco <ip4c0@icloud.com>
2022-05-27 20:45:38 -0500

    Space Tour| Navbar Refactor

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import HamburgerIcon from '../UI/Icons/HamburgerIcon'
 import LogoIcon from '../UI/Icons/LogoIcon'
 
+const menu = [
+  { tittle: 'Home', path: '/' },
+  { tittle: 'Destination', path: '/destination' },
+  { tittle: 'Crew', path: '/crew' },
+  { tittle: 'Technology', path: '/technology' },
+]
+
 function Navbar() {
+  const router = useRouter()
+
   return (
     <div className="absolute top-0 left-0 flex w-full items-center justify-between p-6 lg:top-[40px] lg:px-[55px]">
       <div>
@@ -12,23 +23,25 @@ function Navbar() {
         <HamburgerIcon />
       </div>
       <div className="z-10 ml-[55px] mr-[725px] hidden h-[1px] w-full bg-white/50 lg:inline"></div>
-      <nav className="absolute right-0 top-0 hidden h-[96px] space-x-12 bg-primary-800 px-[46px] font-secondary text-[14px] uppercase tracking-[2.36px] text-white md:flex md:items-center lg:bg-[#979797]/20 lg:pr-[165px] lg:pl-[123px] lg:text-[16px] lg:backdrop-blur-md">
-        <div className="flex h-full items-center border-b-[3px] border-transparent transition-all duration-300 ease-out hover:cursor-pointer hover:border-b-white/50 active:border-b-white">
-          <span className="mr-[11px] hidden font-bold lg:inline-block">00</span>
-          <a href="">Home</a>
-        </div>
-        <div className="flex h-full items-center border-b-[3px] border-transparent transition-all duration-300 ease-out hover:cursor-pointer hover:border-b-white/50 active:border-b-white">
-          <span className="mr-[11px] hidden font-bold lg:inline-block">01</span>
-          <a href="">Destination</a>
-        </div>
-        <div className="flex h-full items-center border-b-[3px] border-transparent transition-all duration-300 ease-out hover:cursor-pointer hover:border-b-white/50 active:border-b-white">
-          <span className="mr-[11px] hidden font-bold lg:inline-block">02</span>
-          <a href="">Crew</a>
-        </div>
-        <div className="flex h-full items-center border-b-[3px] border-transparent transition-all duration-300 ease-out hover:cursor-pointer hover:border-b-white/50 active:border-b-white">
-          <span className="mr-[11px] hidden font-bold lg:inline-block">03</span>
-          <a href="">Technology</a>
-        </div>
+      <nav className="absolute right-0 top-0 hidden h-[96px] space-x-12 bg-primary-800 px-[46px] font-secondary text-[14px] uppercase tracking-[2.36px] text-white md:flex md:items-center lg:bg-[#979797]/20 lg:pr-[165px] lg:pl-[123px] lg:text-[16px] lg:backdrop-blur-sm">
+        {menu.map((item, index) => {
+          return (
+            <Link href={item.path} key={index}>
+              <div
+                className={`flex h-full items-center border-b-[3px] border-transparent transition-all duration-300 ease-out hover:cursor-pointer ${
+                  router.pathname === item.path
+                    ? 'border-b-white'
+                    : 'hover:border-b-white/50'
+                }`}
+              >
+                <span className="mr-[11px] hidden font-bold lg:inline-block">
+                  0{index}
+                </span>
+                <a>{item.tittle}</a>
+              </div>
+            </Link>
+          )
+        })}
       </nav>
     </div>
   )
---
+++ pages/destination/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+function Destination() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Destination
