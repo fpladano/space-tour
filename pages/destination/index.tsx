cd3e132122ba3fa2e94a10cc06210f4c9d42223d
Francisco <ip4c0@icloud.com>
2022-05-27 21:24:55 -0500

    Space Tour | Destination Page

--- pages/destination/index.tsx
+++ pages/destination/index.tsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/Navbar/Navbar'
 
 function Destination() {
   return (
-    <div>
+    <div className="min-w-screen min-h-screen bg-[url('/assets/destination/background-destination-mobile.jpg')] bg-cover p-6 md:bg-[url('/assets/destination/background-destination-tablet.jpg')] md:px-[162px] lg:bg-[url('/assets/destination/background-destination-desktop.jpg')]">
       <Navbar />
     </div>
   )
