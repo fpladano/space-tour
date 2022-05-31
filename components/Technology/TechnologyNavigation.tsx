3dda595843b47a9fa01d42094c8ad1e144d3d8e6
Francisco <ip4c0@icloud.com>
2022-05-31 12:24:15 -0500

    Space Tour | Technology Page Desktop

--- components/Technology/TechnologyNavigation.tsx
+++ components/Technology/TechnologyNavigation.tsx
@@ -15,14 +15,14 @@ function TechnologyNavigation({ menu, active, onClick }: Props) {
   }
 
   return (
-    <nav className="flex justify-center space-x-4">
+    <nav className="flex justify-center space-x-4 lg:flex-col lg:space-x-0 lg:space-y-8">
       {menu.map((item, index) => {
         return (
           <button
             key={index}
             value={item}
             onClick={onClickHanlder}
-            className={`rounded-full border-[1px] border-white/50 px-[18.5px] py-[11px] font-primary md:px-[26px] md:py-[14px] md:text-[24px]   ${
+            className={`rounded-full border-[1px] border-white/50 px-[18.5px] py-[11px] font-primary hover:border-white md:px-[26px] md:py-[14px] md:text-[24px] lg:px-[35.5px] lg:py-[20px] lg:text-[32px]   ${
               active === item
                 ? 'bg-white text-black'
                 : 'bg-transparent text-white'
--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import SectionTitle from '../../components/UI/SectionTitle'
+import TechnologyNavigation from '../../components/Technology/TechnologyNavigation'
+import Image from 'next/image'
+
 import data from './technologyData.json'
 import vehicleImgLandscape from '../../public/assets/technology/image-launch-vehicle-landscape.jpg'
 import spaceportImgLandscape from '../../public/assets/technology/image-spaceport-landscape.jpg'
 import capsuleImgLandscape from '../../public/assets/technology/image-space-capsule-landscape.jpg'
-import Image from 'next/image'
-import TechnologyNavigation from '../../components/Technology/TechnologyNavigation'
+import vehicleImgPortraid from '../../public/assets/technology/image-launch-vehicle-portrait.jpg'
+import spaceportImgPortraid from '../../public/assets/technology/image-spaceport-portrait.jpg'
+import capsuleImgPortraid from '../../public/assets/technology/image-space-capsule-portrait.jpg'
 
 const technologyData = [
-  { ...data[0], landscape: vehicleImgLandscape },
-  { ...data[1], landscape: spaceportImgLandscape },
-  { ...data[2], landscape: capsuleImgLandscape },
+  { ...data[0], landscape: vehicleImgLandscape, portraid: vehicleImgPortraid },
+  {
+    ...data[1],
+    landscape: spaceportImgLandscape,
+    portraid: spaceportImgPortraid,
+  },
+  { ...data[2], landscape: capsuleImgLandscape, portraid: capsuleImgPortraid },
 ]
 
 const menu = technologyData.map((element) => element.name)
@@ -30,28 +38,33 @@ function index() {
       <div className="pt-[88px] md:pt-[136px] md:pl-[38.5px]">
         <SectionTitle />
       </div>
-      <main className="flex flex-col items-center space-y-6 md:space-y-12">
-        <div className="w-full">
+      <main className="flex flex-col items-center space-y-6 md:space-y-12  lg:flex-row-reverse lg:justify-between lg:space-y-0 lg:pl-[50px]">
+        <div className="w-full lg:hidden">
           <Image src={technology.landscape} layout="responsive" />
         </div>
+        <div className="hidden lg:inline-block">
+          <Image src={technology.portraid} />
+        </div>
 
-        <TechnologyNavigation
-          menu={menu}
-          active={technology.name}
-          onClick={onNavigationClickHandler}
-        />
+        <div className="space-y-6 md:space-y-12 lg:flex lg:space-y-0 lg:space-x-8">
+          <TechnologyNavigation
+            menu={menu}
+            active={technology.name}
+            onClick={onNavigationClickHandler}
+          />
 
-        <section className="max-w-[500px] px-6 text-center">
-          <h2 className="mb-[9px] font-secondary text-[14px] uppercase tracking-[2.36px] text-secondary-100 md:text-[16px] md:tracking-[2.7px]">
-            The Terminology...
-          </h2>
-          <h1 className="mb-[16px] font-primary text-[24px] uppercase text-white md:text-[40px]">
-            {technology.name}
-          </h1>
-          <p className="font-paragraph text-[15px] leading-[25px] text-secondary-100 md:text-[16px] md:leading-[28px]">
-            {technology.description}
-          </p>
-        </section>
+          <section className="max-w-[500px] px-6 text-center lg:max-w-[600px] lg:text-justify">
+            <h2 className="mb-[9px] font-secondary text-[14px] uppercase tracking-[2.36px] text-secondary-100 md:text-[16px] md:tracking-[2.7px]">
+              The Terminology...
+            </h2>
+            <h1 className="mb-[16px] font-primary text-[24px] uppercase text-white md:text-[40px] lg:text-[56px]">
+              {technology.name}
+            </h1>
+            <p className="font-paragraph text-[15px] leading-[25px] text-secondary-100 md:text-[16px] md:leading-[28px] lg:text-[18px] lg:leading-[32px]">
+              {technology.description}
+            </p>
+          </section>
+        </div>
       </main>
     </div>
   )
